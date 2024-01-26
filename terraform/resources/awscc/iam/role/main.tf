efdb2d55118a38ddc122ebba84a1b8808e5e8ed6
unchaptered <workstation19961002@gmail.com>
2024-01-26 16:48:48 +0900

    Simplify

--- terraform/resources/awscc/iam/role/main.tf
+++ terraform/resources/awscc/iam/role/main.tf
@@ -1,4 +1,4 @@
-resource "awscc_iam_role" "awscc_iam_role_module" {
+resource "awscc_iam_role" "resource" {
   role_name                   = var.role_name # "ChatBot-Channel-Role"
   assume_role_policy_document = jsonencode(var.assume_role_policy_document)
   managed_policy_arns         = var.managed_policy_arns
