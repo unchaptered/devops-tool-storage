e1b25392f61c7b80b32191819f2181f0a731a87f
unchaptered <workstation19961002@gmail.com>
2024-01-31 11:05:53 +0900

    Create : TF | Modules | storages/public_storage

--- terraform/modules/aws/storages/public_storage/s3.acl.tf
+++ terraform/modules/aws/storages/public_storage/s3.acl.tf
@@ -1,7 +1,7 @@
 module "s3_acl" {
   source = "../../../../resources/aws/s3/acl"
 
-  bucket     = module.s3_bucket.bucket
   acl        = "public-read"
-  depends_on = [module.s3_bucket.bucket]
+  bucket_id  = module.s3_bucket.id
+  depends_on = [module.s3_bucket]
 }
--- terraform/modules/aws/storages/public_storage/variables.tf
+++ terraform/modules/aws/storages/public_storage/variables.tf
@@ -5,3 +5,34 @@ variable "bucket_name" {
 variable "tags" {
   type = map(any)
 }
+variable "prefix" {
+  type        = string
+  description = <<-DESCRIPTION
+  Generally, prefix contains "service" and "stage"
+
+  - service must be 2~5, for example : "kevin"
+  - stage must be 2~5, for example : ["prod", "dev", "stage", "test"]
+  - prefix for examples : ["kevin-prod", "kevin-dev", "kevin-stage", "kevin-test"]
+  DESCRIPTION
+
+  validation {
+    condition     = can(regex("^[a-z\\-]{4,11}$", var.prefix))
+    error_message = "var.prefix must be 10 with lowercase eng"
+  }
+}
+
+variable "suffix" {
+  type        = string
+  description = <<-DESCRIPTION
+  Generally, suffix contains "region_name"
+
+  - regaion_name
+  - Best : "ap-ne-2"
+  - Worst : "ap-northeast-2"
+  DESCRIPTION
+
+  validation {
+    condition     = can(regex("^[a-z0-9\\-]{4,71}$", var.suffix))
+    error_message = "var.suffix must be 10 with lowercase eng"
+  }
+}
