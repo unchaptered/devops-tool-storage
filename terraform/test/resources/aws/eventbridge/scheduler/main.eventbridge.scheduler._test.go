53d41f0b094e7c9833c3f68f4df4bbdc1925cce0
unchaptered <workstation19961002@gmail.com>
2024-02-13 23:12:18 +0900

    Test : TF | Resources | EventBridge Scheduler

---
+++ terraform/test/resources/aws/eventbridge/scheduler/main.eventbridge.scheduler._test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func Test_Terraform_Resource_Aws_Eventbridge_Scheduler_Component(t *testing.T) {
+	tfOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../../../resources/aws/eventbridge/scheduler",
+	})
+
+	defer terraform.Destroy(t, tfOptions)
+
+	terraform.Init(t, tfOptions)
+}
