96cbfa771a7aceae2616f3c89dd05a822f981c4a
unchaptered <workstation19961002@gmail.com>
2024-02-13 23:45:33 +0900

    Test : TF | Resources | SM Secret

---
+++ terraform/test/resources/aws/sm/secret/main.sm.secret._test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func Test_Terraform_Resource_Aws_Sm_Secret_Component(t *testing.T) {
+	tfOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../../../resources/aws/sm/secret",
+	})
+
+	defer terraform.Destroy(t, tfOptions)
+
+	terraform.Init(t, tfOptions)
+}
