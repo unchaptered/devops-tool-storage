6a977485ef809c14456c7256fe03de82accef049
unchaptered <workstation19961002@gmail.com>
2024-02-17 16:25:23 +0900

    Rename : TF | Module | AWS SecureStorage to BasicStorage

--- terraform/modules/aws/storages/README.md
+++ terraform/modules/aws/storages/README.md
@@ -5,7 +5,7 @@
 TF Storage Modules support to deploy serverless modules.
 
 - [Asset Storage](./asset_storage/README.md)
-- [Basic Storage](./basic_stroages/README.md)
+- [Basic Storage](./basic_stroage/README.md)
 - [Website Storage](./website_storage/README.md)
 - [Video Complex Storage](./video_complex_storage/README.md)
 - [Video Simple Storage](./video_simple_storage/README.md)
--- terraform/test/modules/aws/storages/asset_stoarge/main.storages.asset_storage._test.go
+++ terraform/test/modules/aws/storages/asset_stoarge/main.storages.asset_storage._test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func Test_Terraform_Modules_Aws_Storages_Asset_storage(t *testing.T) {
+func Test_Terraform_Modules_Aws_Storages_AssetStorage(t *testing.T) {
 	tfOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../../../../../modules/aws/storages/asset_storage",
 		VarFiles:     []string{"./sample.tfvars"},
