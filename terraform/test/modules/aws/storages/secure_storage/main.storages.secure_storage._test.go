c555e6d703a1befc35323cc6ce15eb1a32cca351
unchaptered <workstation19961002@gmail.com>
2024-02-17 16:16:11 +0900

    Test : TF | Module | Aws AssetStorage,SecureStorage

---
+++ terraform/test/modules/aws/storages/secure_storage/main.storages.secure_storage._test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func Test_Terraform_Modules_Aws_Storages_Secure_storage(t *testing.T) {
+	tfOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../../../../modules/aws/storages/secure_storage",
+		VarFiles:     []string{"./sample.tfvars"},
+	})
+
+	defer terraform.Destroy(t, tfOptions)
+
+	terraform.Init(t, tfOptions)
+}
