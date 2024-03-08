b6e85ebf6301b50ad5f942a0d2635b78347be109
unchaptered <workstation19961002@gmail.com>
2024-03-08 17:09:01 +0900

    Update : TF | Resource | EKS Cluster Outputs

--- terraform/resources/aws/eks/cluster/main.tf
+++ terraform/resources/aws/eks/cluster/main.tf
@@ -14,29 +14,3 @@ resource "aws_eks_cluster" "resource" {
         endpoint_public_access = var.vpc_config.endpoint_public_access
     }
 }
-
-variable "name" {
-    type = string
-}
-variable "role_arn" {
-    type = string
-    sensitive = true
-}
-
-variable "vpc_config" {
-    type = object({
-        subnet_ids = list(string)
-        security_group_ids = list(string)
-        public_access_cidrs = list(string)
-        endpoint_private_access = bool
-        endpoint_public_access = bool
-    })
-
-    default = {
-        public_access_cidrs = ["0.0.0.0/0"]
-        security_group_ids = []
-
-        endpoint_private_access = false
-        endpoint_public_access = true
-    }
-}
