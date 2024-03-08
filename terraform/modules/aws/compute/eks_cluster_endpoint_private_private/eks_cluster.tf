9b3ec40183d1e81bb2c2dd69e21fb7efceb7db4c
unchaptered <workstation19961002@gmail.com>
2024-03-08 17:24:04 +0900

    Create : TF | Modules | AWS EKS Cluster with Private Private Endpoint

---
+++ terraform/modules/aws/compute/eks_cluster_endpoint_private_private/eks_cluster.tf
@@ -0,0 +1,22 @@
+module "eks_cluster" {
+  source = "../../../../resources/aws/eks/cluster"
+
+  name     = "${var.prefix}-eks-cluster-${var.eks_cluster_params.name}-${var.suffix}"
+  role_arn = module.eks_cluster_iam_role.arn
+  vpc_config = {
+    subnet_ids          = var.eks_cluster_params.subnet_ids
+    security_group_ids  = var.eks_cluster_params.security_group_ids
+    public_access_cidrs = var.eks_cluster_params.public_access_cidrs
+
+    endpoint_public_access  = false
+    endpoint_private_access = true
+  }
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks_cluster.endpoint
+}
+
+output "eks_cluster_certificate_authority" {
+  value = module.eks_cluster.certificate_authority
+}
